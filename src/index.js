5d23edccf4d5c834298b226872755abc5e0325fe
Gabe Velasquez <gvelasquez@trackforce.com>
2019-01-17 14:14:01 -0800

    messing with state!

--- src/index.js
+++ src/index.js
@@ -2,8 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+  //js function, NOT react
+  constructor(props) {
+    //pass props to base class (react.component)
+    //mandatory!!!
+    super(props);
+
+    //this is the ONLY TIME we do direct assignment to this.state
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude
+        });
+      },
+      // dont do this!!!
+      // this.state.lat = position.coords.latitude
+      err => console.log(err)
+    );
+  }
+
+  //we have ot define render!!
   render() {
-    return <div>Latitude: </div>;
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
