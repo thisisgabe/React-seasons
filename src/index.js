1d5823083c740d86e74d2852f14c7fdce9758591
Gabe Velasquez <gvelasquez@trackforce.com>
2019-01-17 15:59:42 -0800

    basic season page working based on latitude from geolocation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   //js function, NOT react
@@ -31,7 +32,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Lattitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading...</div>;
